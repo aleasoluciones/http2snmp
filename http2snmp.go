3a10b8b3cb849efbd1b4bd5ad6751e8cfb693206
Eduardo Ferro <eduardo.ferro.aldama@gmail.com>
2014-11-30 17:09:49 +0100

    Initial code (WIP)

---
+++ http2snmp.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/aleasoluciones/gosnmpquerier"
+)
+
+const (
+	CONTENTION = 4
+)
+
+func rootHandler(querier *gosnmpquerier.SyncQuerier, w http.ResponseWriter, r *http.Request) {
+
+	cmd, _ := gosnmpquerier.ConvertCommand(r.FormValue("cmd"))
+	query := gosnmpquerier.Query{
+		Cmd:         cmd,
+		Community:   r.FormValue("community"),
+		Oids:        []string{r.FormValue("oid")},
+		Timeout:     time.Duration(10) * time.Second,
+		Retries:     1,
+		Destination: r.FormValue("destination"),
+	}
+	processed := querier.ExecuteQuery(query)
+	jsonProcessed, err := gosnmpquerier.ToJson(&processed)
+	if err != nil {
+		fmt.Fprint(w, err)
+	}
+	fmt.Fprint(w, jsonProcessed)
+
+}
+
+func main() {
+
+	querier := gosnmpquerier.NewSyncQuerier(CONTENTION, 3, 3*time.Second)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		rootHandler(querier, w, r)
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
